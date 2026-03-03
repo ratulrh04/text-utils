554b972956706327fb91a5ca0125f22392cc870f
Ratul-Raha <ratulraha.04@gmail.com>
2026-03-04 04:53:19 +0600

    app page some bug fixig

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,30 @@ import { useState } from "react";
 import Accordian from "./components/Accordian";
 import Nav from "./components/Nav";
 import TextArea from "./components/TextArea";
-import Alert from "./components/Alert"; // Uncommented this line
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NoPage from "./components/NoPage"; // Assuming you will define or import this
+import Alert from "./components/Alert";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <-- changed
+import NoPage from "./components/NoPage";
 
 const App = () => {
-  // Navbar props
   let navItems = {
     item1: 'home',
     item2: 'Text-Utils',
     item3: 'About-Table',
   };
 
-  // TextArea heading props
   let textHeading = {
     heading: 'This is Text Area',
   };
 
-  // Navbar dark mode
-  const [mode, setMode] = useState('dark'); // Default mode: dark
-  const [bodyMode, setBodyMode] = useState('#1c0238'); // Background color
+  const [mode, setMode] = useState('dark');
+  const [bodyMode, setBodyMode] = useState('#1c0238');
   const [alert, setAlert] = useState(null);
 
-  // Alert function
   let showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
+    setAlert({ msg: message, type });
+    setTimeout(() => setAlert(null), 1500);
   };
 
-  // Navbar switch mode function
   let switchMode = () => {
     if (mode === 'light') {
       setMode('dark');
@@ -51,7 +41,7 @@ const App = () => {
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter> {/* <-- changed */}
       <Nav {...navItems} mode={mode} switchMode={switchMode} />
       <Alert alert={alert} />
       <Routes>
@@ -60,7 +50,7 @@ const App = () => {
         <Route path="/about-table" element={<Accordian />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
